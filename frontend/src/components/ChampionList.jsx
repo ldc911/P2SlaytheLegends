49227a0ad1efca77db177e2eaf92277a19524395
unknown <ndjbouman@gmail.com>
2022-10-21 12:19:18 +0200

    test de git

---
+++ frontend/src/components/ChampionList.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import api from "../services/api";
+
+function ChampionsList() {
+  const [champions, setChampions] = useState([]);
+  const [isMounting, setIsMounting] = useState(true);
+
+  useEffect(() => {
+    api.getChampions().then((json) => {
+      setChampions(json.data);
+      setIsMounting(false);
+    });
+  }, []);
+
+  return isMounting ? (
+    <p>En cours de chargement =P</p>
+  ) : (
+    <ul>
+      {champions &&
+        Object.entries(champions).map((champion) => {
+          return (
+            <li key={champion[1].id}>
+              {champion[1].stats.hp} {champion[1].title}
+              <img
+                alt={champion[1].id}
+                src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion[1].id}_0.jpg`}
+              />
+            </li>
+          );
+        })}
+    </ul>
+  );
+}
+
+export default ChampionsList;
