8ff09db867d921a1802fda45b06c1ef9f7b5b7a1
unknown <ndjbouman@gmail.com>
2022-10-27 12:15:42 +0200

    background random sur Home operationnel + App.css mise à jour

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,20 +1,23 @@
 import Home from "../pages/Home";
 import "../assets/css/App.css";
+import api from "@services/api";
 
 // random image background
 function pic() {
-  // http://ddragon.leagueoflegends.com/cdn/img/champion/splash/
-
-  const bgm = [
-    "http://i.imgur.com/Z0qJG.png",
-    "http://i.imgur.com/AOkCD.png",
-    "https://static.planetminecraft.com/files/resource_media/screenshot/1143/2011-10-30_131700_732517.jpg",
-  ];
-  document.body.style.background = `no-repeat url(${
+  let splashs = [];
+  api.getChampions().then((json) => {
+    //recupere la liste json
+    splashs.push(json.data);
+    // met les premiere keys de json dans un array
+    const bgm = Object.keys(splashs[0]);
+    // Change le style background
+    document.body.style.background = `no-repeat fixed url(http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${
     bgm[Math.floor(Math.random() * bgm.length)]
-  }) `;
+    }_0.jpg) 50% 50% / cover #ffffff`;
+  });
 }
 pic();
+
 
 function App() {
   return (
