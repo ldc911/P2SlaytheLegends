fbe7a66b519ae0636c4f500a5cbf8c0ee8b4db14
Romain Timmer <timmer.romain@free.fr>
2022-10-27 16:12:42 +0200

    Creation Dark and Light Themes and Theme Button

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,6 +1,6 @@
 import Home from "@pages/Home";
 import "@assets/css/App.css";
-//import useLocalStorage from "use-local-storage";
+// import useLocalStorage from "use-local-storage";
 import React from "react";
 
 function App() {
