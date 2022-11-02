82bd45d2759fbf999112b96605ea6a43f0626c63
Laurent Duclos <lduclos@protonmail.com>
2022-11-02 21:00:08 +0100

    fix by npm run fix

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,6 @@ import Footer from "@components/Footer/Footer";
 import MainContent from "@components/MainContent/MainContent";
 import HeaderHome from "@components/Header/HeaderHome";
 
-
 export default function Home() {
   return (
     <>
